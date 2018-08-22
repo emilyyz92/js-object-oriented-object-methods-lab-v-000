3ec822202e725543fcfbb81d49e85548937fa330
Emily Zhao <tong.zhao.92@gmail.com>
2018-08-22 21:08:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,4 +14,7 @@ const BoardMember = function (name, homestate, training) {
   this.releasePressStatement = function () {
     return "You will see great things from Scuber.";
   }
+  this.sayHi = function () {
+    return `Hi, my name is ${this.name}. I am from New York, and I was trained in law.`
+  }
 }
