d8acf697ed757a3102d40c75d340cfb5c50b21c9
Emily Zhao <tong.zhao.92@gmail.com>
2018-08-22 21:08:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,6 +15,6 @@ const BoardMember = function (name, homestate, training) {
     return "You will see great things from Scuber.";
   }
   this.sayHi = function () {
-    return `Hi, my name is ${this.name}. I am from New York, and I was trained in law.`
+    return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`;
   }
 }
