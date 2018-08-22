06c2b8e51aae401099cddf00c09d7d1970de2708
Emily Zhao <tong.zhao.92@gmail.com>
2018-08-22 20:53:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,4 +2,10 @@ const BoardMember = function (name, homestate, training) {
   this.name = name;
   this.homestate = homestate;
   this.training = training;
+  this.veto = function () {
+    return `No, I must disagree`;
+  };
+  this.approve = function () {
+    return `You can do that!`
+  };
 }
